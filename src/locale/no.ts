3ca53eb09dedee6319ebdf99a2702894458ba9b2
Jørgen Stamnes <jorgenstamnes@Jrgens-MacBook-Pro.local>
2019-11-16 11:44:21 +0100

    type title

--- src/locale/no.ts
+++ src/locale/no.ts
@@ -11,7 +11,7 @@ export default {
       title: "Sosiale aktiviteter"
     },
     training: {
-      title: "våre treninger"
+      title: "Våre treninger"
     },
     board: {
       title: "Styret"
